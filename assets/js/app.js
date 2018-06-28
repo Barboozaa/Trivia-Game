317564b4854cb0c5e64a8a187265a3827235aa5d
inkspot <Wyattsimpson777@gmail.com>
2018-06-28 18:38:09 -0400

    pseudocode done, skeleton html

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+// Game will need to be written in a way that it can be played again without refreshing the page*** 
+
+// Display start button
+    // when hit, show question, answers, and timer (30 seconds)
+
+// Display question, answer choices, and timer
+    // Timer Ticks at 1 second intervals, put reset so interval won't compound
+    // if it hits 0, stop at 0, say "Time's Up", display correct answer
+    // if wrong, say "Wrong" and show right answer
+    // if right, say "Correct"
+    // Right, wrong, or timeout, pause the clock and wait 5~ seconds. Then move to next question (WITHOUT user input)
+
+// Game Ends
+    // Display number of correct and incorrect answers
+    // Display timeouts (if there are none, don't. Idk how this will work out, but good luck)
+    // ***Play again button that DOES NOT refresh the page, just starts the came over***
+
+document.getElementById("choice1").innerHTML = "js test";
+
+$(".question").text("jQ test");
+
+console.log("js console test");
+
+$(document).ready(function() {
+    console.log("jQ console test");
+});
