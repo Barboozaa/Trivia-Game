8764461368d8d8fb31560460330364e31862db4a
inkspot <Wyattsimpson777@gmail.com>
2018-07-02 23:52:08 -0400

    If you wouldve told me that JavaScript would have this kind of effect on my mental health, I wouldve just saved the the money for this class and fucking killed myself

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,107 +1,156 @@
-$(document).ready(function() {
-    console.log("Hola Binches");
-// Game will need to be written in a way that it can be played again without refreshing the page*** 
+$(document).ready(function () {
 
-// Global Vars :
-var startButtonPressed = false;
-console.log("SBP: " + startButtonPressed);
+    $(".startButton").on("click", function () {
+        game.startGame();
+    });
 
-var questions = {
-    "q1": "qwerty", 
-    "q2": "qwerty", 
-    "q3": "qwerty", 
-    "q4": "qwerty", 
-    "q5": "qwerty"};
+    $(document).on("click", ".buttons", function () {
+        game.guessChecker();  
+        console.log($(this).text());
+    });
 
-var answers = {
-    "q1":["1", "2", "3", "4"],
-    "q2":["1", "2", "3", "4"],
-    "q3":["1", "2", "3", "4"],
-    "q4":["1", "2", "3", "4"],
-    "q5":["1", "2", "3", "4"]
-};
+    var game = {
 
-var correctAnswers = {
-    "q1":"1",
-    "q2":"1",
-    "q3":"1",
-    "q4":"1",
-    "q5":"1"
-};
+        questions: {
+            q1: "Which of the following is NOT a class?",
+            q2: "Which school of magic is used to tell fortunes?",
+            q3: "What is the most common enemy for low-leveled characters to fight?",
+            q4: "Which of these races is known for their short but stocky builds, a penchant for blacksmithing, and great big beards?",
+            q5: "Which of the following is an actual technique a Monk might use?"
+        },
 
-var timerRunning = false;
+        answers: {
+            q1: ["Fighter", "Druid", "Cook", "Monk"],
+            q2: ["Divination", "Necromancy", "Conjuration", "Lying"],
+            q3: ["Dragons", "A Literal Army", "Goblins", "Old People"],
+            q4: ["Elves", "Halflings", "Dwarves", "Orcs"],
+            q5: ["Punchy Style", "Really Hard Kick", "Stunning Fist", "Super Fist of the Nose Hair"]
+        },
 
-var timerInterval;
+        correctAnswers: {
+            q1: "Cook",
+            q2: "Divination",
+            q3: "Goblins",
+            q4: "Dwarves",
+            q5: "Stunning Fist"
+        },
 
-var timer = {
+        currentQuestion: 0,
 
-    time: 10,
+        correct: 0,
 
-    count: function() {
-        $(".timer").text("Timer : " + timer.time); 
-        if (timer.time != 0) {
-            timer.time--;            
-        } else {
-            timer.stop();
-            $(".timer").text("Time's Up!")            
+        incorrect: 0,
+
+        unanswered: 0,
+
+        timerRunning: false,
+
+        timerInterval: "",
+
+        timeOut: false,
+
+        startGame: function () {
+            game.currentQuestion = 0;
+
+            clearInterval(game.timerInterval);
+
+            $(".contentWrapper").show();
+
+            $(".rightWrong").empty();
+            $(".question").empty();
+            $("#choices").empty();
+            $(".results").empty();
+
+            $(".timer").text(game.timer);
+
+            $(".startButton").hide();
+
+            game.nextQuestion();
+        },
+
+        nextQuestion: function () {
+
+            game.timer = 10;
+
+            $(".timer").text(game.timer);
+
+            if (!game.timerOn) {
+                game.timerInterval = setInterval(game.timerRunning, 1000);
+            }
+
+            var questionText = Object.values(game.questions)[game.currentQuestion];
+            $(".question").text(questionText);
+
+            var questionChoices = Object.values(game.answers)[game.currentQuestion];
+
+            $.each(questionChoices, function (index, key) {
+                $("#choices").append($("<button class='buttons'>" + key + "</button>"));
+            })
+        },
+
+        timerRunning: function () {
+            if (game.timer > -1 && game.currentQuestion < Object.keys(game.questions).length) {
+                $(".timer").text(game.timer);
+                game.timer--;
+            } else if (game.timer === -1) {
+                game.unanswered++;
+                game.result = false;
+                clearInterval(game.timerInterval);
+                $(".timer").empty();
+                $(".question").empty();
+                $("#choices").empty();
+                resultTimeout = setTimeout(game.guessResult, 5000);
+                $(".rightWrong").text("Out of time! The answer was " + Object.values(game.correctAnswers)[game.currentQuestion]);
+            }
+            else if (game.currentQuestion === Object.keys(game.questions).length) {
+
+                $(".results").html(
+                    "<h3>Check out your loot</h3>" +
+                    "<p>Correct: " + game.correct + "</p>" +
+                    "<p>Incorrect: " + game.incorrect + "</p>" +
+                    "<p>Unaswered: " + game.unanswered + "</p>"
+                );
+                $(".question").empty();
+                $(".timer").empty();
+                $(".startButton").show();
+            }
+        },
+
+        guessChecker: function () {
+
+            var resultTimeout;
+
+            var currentAnswer = Object.values(game.correctAnswers)[game.currentQuestion];
+            
+            var input = $(this).text();
+
+            if (input === currentAnswer) {
+                game.correct++;
+                clearInterval(game.timerInterval);
+                $(".timer").empty();
+                $(".question").empty();
+                $("#choices").empty();
+                resultTimeout = setTimeout(game.guessResult, 5000);
+                $(".rightWrong").html("Correct!");
+            } else {
+                game.incorrect++;
+                clearInterval(game.timerInterval);
+                $(".timer").empty();
+                $(".question").empty();
+                $("#choices").empty();
+                resultTimeout = setTimeout(game.guessResult, 5000);
+                $(".rightWrong").html("Wrong! The answer was " + currentAnswer);
+            }
+        },
+
+        guessResult: function () {
+
+            game.currentQuestion++;
+
+            $("#choices").empty();
+            $(".rightWrong").empty();
+
+            game.nextQuestion();
         }
-      },
-    
-    start: function() {
-        if (!timerRunning) {
-            timerRunning = true;
-            timerInterval = setInterval(timer.count, 1000);
-            timer.count();
-        };
-    },
-
-    stop: function() {
-        clearInterval(timerInterval);
-        timerRunning = false;
-        time = 30;
     }
-};
-
-// Display start button
-    // when hit, show question, answers, and timer (30 seconds)
-$(".startButton").on("click", function() {
-    // Hides start button
-    startButtonPressed = true;
-    $(".startButton").hide();
-    console.log("SBP: " + startButtonPressed);
-
-    // Call gameSet function
-    gameSet();
 });
-
-function gameSet() {
-    timer.start();
-    $(".question").text(questions.q1)
-    answers.q1.forEach(function(x) {
-        var choice = $("<p>");
-        choice.text(x);
-        $("#choices").append(choice);
-    })
-};
-
-// Display question, answer choices, and timer
-    // Timer Ticks at 1 second intervals, put reset so interval won't compound
-    // if it hits 0, stop at 0, say "Time's Up", display correct answer
-    // if wrong, say "Wrong" and show right answer
-    // if right, say "Correct"
-    // Right, wrong, or timeout, pause the clock and wait 5~ seconds. Then move to next question (WITHOUT user input)
-
-// Game Ends
-    // Display number of correct and incorrect answers
-    // Display timeouts (if there are none, don't. Idk how this will work out, but good luck)
-    // ***Play again button that DOES NOT refresh the page, just starts the came over***
-
-
-    //document.ready is needed for it to work in the head tag
-// document.getElementById("choice1").innerHTML = "js test";
-
-// $(".question").text("jQ test");
-
-// console.log("js console test");
-
-}); // ends the document ready
